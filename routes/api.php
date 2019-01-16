ff2207793b48d0fde78b03474bbf51852ff081a2
1032380143 <1032380143@qq.com>
2019-01-16 13:36:14 +0800

    添加微信认证

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::namespace('Api')->middleware(['web','wechat.oauth'])->group(function(){
+    Route::apiResource('sign','SignsController');
+});
