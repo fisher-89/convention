0bbe36d4ef06f4a15eb52a775ea6a5382c111b7a
1032380143 <1032380143@qq.com>
2019-01-18 09:51:09 +0800

    web路由获取当前用户删除

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::any('/wechat', 'WeChatController@serve');
-
-
-Route::group(['middleware' => ['web','wechat.oauth']], function () {
-    Route::get('/user', function () {
-        $user = session('wechat.oauth_user.default'); // 拿到授权用户资料
-
-        dd($user);
-    });
-});
