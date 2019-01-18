abefd633feac48dfc630693cd6245786ee8bfd67
1032380143 <1032380143@qq.com>
2019-01-18 09:38:54 +0800

    web路由获取当前用户

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,9 @@ Route::get('/', function () {
 Route::any('/wechat', 'WeChatController@serve');
 
 
-Route::group(['middleware' => ['web', 'wechat.oauth:snsapi_userinfo']], function () {
+Route::group(['middleware' => ['web', 'wechat.oauth']], function () {
     Route::get('/user', function () {
-        $user = session('wechat.oauth_user'); // 拿到授权用户资料
+        $user = session('wechat.oauth_user.default'); // 拿到授权用户资料
 
         dd($user);
     });
