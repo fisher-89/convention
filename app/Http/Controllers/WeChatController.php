77ed5776aca82b49795e2ebffba56f2200ae0dd7
1032380143 <1032380143@qq.com>
2019-01-21 15:12:07 +0800

    测试

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -27,11 +27,26 @@ class WeChatController extends Controller
 
     public function show()
     {
-        $openPlatform = app('wechat.open_platform');
+//        $openPlatform = app('wechat.open_platform');
 //        $openPlatform = Factory::openPlatform(config('wechat.open_platform.default'));
-        $openPlatform->getPreAuthorizationUrl(route('callback')); // 传入回调URI即可
+//        $openPlatform->getPreAuthorizationUrl(route('callback')); // 传入回调URI即可
 
-        dd($openPlatform);
+        $url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx928eacf7fdbd4e0d&redirect_uri='.urlencode(route('callback')).'&response_type=code&scope=snsapi_base';
+
+          //初始化
+      $curl = curl_init();
+      //设置抓取的url
+      curl_setopt($curl, CURLOPT_URL, $url);
+      //设置头文件的信息作为数据流输出
+      curl_setopt($curl, CURLOPT_HEADER, 1);
+      //设置获取的信息以文件流的形式返回，而不是直接输出。
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+      //执行命令
+     $data = curl_exec($curl);
+     //关闭URL请求
+     curl_close($curl);
+     //显示获得的数据
+     dd($data);
     }
 
     public function callback()
