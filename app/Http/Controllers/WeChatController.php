104f4b37f24676640244a3f5fa53853a1209b10d
1032380143 <1032380143@qq.com>
2019-02-18 17:22:56 +0800

    测试微信code的回调地址

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -28,7 +28,7 @@ class WeChatController extends Controller
 
     public function checkSign()
     {
-        $redirectUri = 'http://cs.xigemall.com';
+        $redirectUri = '/';
         if(Session::has('wechat_user')){
             return redirect($redirectUri);
         }else{
