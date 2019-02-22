e3664677116aa6dd18e7a13cd1c9741b5ffbcac2
1032380143 <1032380143@qq.com>
2019-02-22 11:52:20 +0800

    删除中奖用户 广播添加到test队列

--- app/Events/WinnerAbandon.php
+++ app/Events/WinnerAbandon.php
@@ -14,6 +14,7 @@ class WinnerAbandon implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
+    public $broadcastQueue = 'test';
     public $data;
     /**
      * Create a new event instance.
