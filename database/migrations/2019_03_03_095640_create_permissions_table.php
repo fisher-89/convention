4a7fc8921d9321e13b5b9290a32426a46d98b0f9
1032380143 <1032380143@qq.com>
2019-03-03 10:04:53 +0800

    添加修改签到数据权限表

---
+++ database/migrations/2019_03_03_095640_create_permissions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permissions', function (Blueprint $table) {
+            $table->engine = 'MyISAM';
+            $table->increments('id');
+            $table->char('staff_sn',10)->comment('工号')->unique();
+            $table->char('realname',20)->comment('名字');
+//            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permissions');
+    }
+}
