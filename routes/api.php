57ed68098f43599f56dd69002de7ed336869aea6
liuyong <1032380143@qq.com>
2019-03-06 19:24:47 +0800

    后台管理员中间件去除不包含补签的相关接口

--- routes/api.php
+++ routes/api.php
@@ -23,46 +23,50 @@ Route::post('sign', 'Api\SignsController@store');
 Route::get('sign/{openid}', 'Api\SignsController@show');
 
 // 检测用户登陆
-Route::get('check/{openid}','Api\SignsController@check');
+Route::get('check/{openid}', 'Api\SignsController@check');
 
-Route::middleware(['auth:api','check.manager'])->group(function () {
+Route::middleware('auth:api')->group(function () {
+
     // 签到列表
     Route::get('sign', 'Api\SignsController@index');
     // 签到补充信息
     Route::patch('sign/{openid}', 'Api\SignsController@update');
-    // 签到数据清空
-    Route::delete('sign_clear','Api\SignsController@signClear');
     // 上传身份证
     Route::post('upload', 'Api\SignsController@upload');
 
-    // 获取配置
-    Route::get('configuration', 'Api\ConfigurationsController@index');
-    // 配置提交
-    Route::post('configuration', 'Api\ConfigurationsController@store');
-    // 配置修改
-    Route::put('configuration/{round}', 'Api\ConfigurationsController@update');
-    // 清空全部配置与中奖信息
-    Route::delete('configuration_clear','Api\ConfigurationsController@configurationClear');
-    // 开始抽奖
-    Route::get('start', 'Api\ConfigurationsController@start');
-    // 停止抽奖
-    Route::get('stop', 'Api\ConfigurationsController@stop');
-    // 继续抽奖
-    Route::get('continue', 'Api\ConfigurationsController@continueDraw');
+    Route::middleware('check.manager')->group(function () {
+        // 签到数据清空
+        Route::delete('sign_clear', 'Api\SignsController@signClear');
 
-    // 奖品
-    Route::apiResource('award', 'Api\AwardsController')->only([
-        'index','store','update','destroy'
-    ]);
-    // 上传奖品
-    Route::post('upload_award','Api\AwardsController@uploadAward');
+        // 获取配置
+        Route::get('configuration', 'Api\ConfigurationsController@index');
+        // 配置提交
+        Route::post('configuration', 'Api\ConfigurationsController@store');
+        // 配置修改
+        Route::put('configuration/{round}', 'Api\ConfigurationsController@update');
+        // 清空全部配置与中奖信息
+        Route::delete('configuration_clear', 'Api\ConfigurationsController@configurationClear');
+        // 开始抽奖
+        Route::get('start', 'Api\ConfigurationsController@start');
+        // 停止抽奖
+        Route::get('stop', 'Api\ConfigurationsController@stop');
+        // 继续抽奖
+        Route::get('continue', 'Api\ConfigurationsController@continueDraw');
 
-    // 中奖
-    Route::apiResource('winner', 'Api\WinnersController')->only([
-        'index'
-    ]);
-    //弃奖
-    Route::patch('abandon_prize', 'Api\WinnersController@abandonPrize');
+        // 奖品
+        Route::apiResource('award', 'Api\AwardsController')->only([
+            'index', 'store', 'update', 'destroy'
+        ]);
+        // 上传奖品
+        Route::post('upload_award', 'Api\AwardsController@uploadAward');
+
+        // 中奖
+        Route::apiResource('winner', 'Api\WinnersController')->only([
+            'index'
+        ]);
+        //弃奖
+        Route::patch('abandon_prize', 'Api\WinnersController@abandonPrize');
+    });
 });
 
 // 大屏获取最新配置抽奖
