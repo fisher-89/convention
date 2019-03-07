6834cafebdea400374b8a3b5e9e57f925a861d28
1032380143 <1032380143@qq.com>
2019-03-07 14:11:42 +0800

    签到表头像字段修改允许为null

--- database/migrations/2019_01_16_023319_create_signs_table.php
+++ database/migrations/2019_01_16_023319_create_signs_table.php
@@ -20,7 +20,7 @@ class CreateSignsTable extends Migration
             $table->char('mobile',11)->unique()->comment('签到手机');
             $table->char('openid',50)->comment('微信openId')->unique();
             $table->char('nickname',50)->comment('微信用户昵称')->default('');
-            $table->string('avatar')->comment('微信头像')->default('');
+            $table->string('avatar')->comment('微信头像')->default('')->nullable();
             $table->unsignedTinyInteger('sex')->comment('微信用户的性别，值为1时是男性，值为2时是女性，值为0时是未知')->default(0);
             $table->char('number',50)->comment('邀请函编号')->index()->nullable()->default('');
             $table->char('hotel_name',50)->comment('酒店名称')->index()->nullable()->default('');
